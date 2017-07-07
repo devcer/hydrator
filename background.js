c204b68be9fde0b3875d446bd93a1cba1bb946ea
viswanathamsantosh <viswanathamsantosh@gmail.com>
2017-07-07 08:50:16 +0530

    changed waiting time for testing

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 var notificationMessage = "Hey buddy, You should drink some water.";
 var notificationTitle = "Stay hidrated!";
-var timeInterval = 15;
+// var timeInterval = 15; 
+var timeInterval = 0.5;
 // var buttons = [
 //   {
 //     "title": "Snooze"
@@ -14,12 +15,14 @@ browser.runtime.onMessage.addListener(handleMessage);
 
 function handleMessage(request, sender, sendResponse) {
   console.log(" Time: " + request.time);
-  timeInterval= request.time;
+  //timeInterval= request.time; 
+  timeInterval= request.time/30;
   sendResponse({response: "Time received successfully"});
   restartAlarms();
 }
 
 browser.alarms.onAlarm.addListener(function(alarm) {
+	console.log(new Date());
 	browser.notifications.create("waterNotification",{
 	  "type": "basic",
 	  "iconUrl": "icons/bottle.png",
