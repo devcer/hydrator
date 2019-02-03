47770aaed3f7f64c05ea158c85adeeffc999de84
viswanathamsantosh <viswanathamsantosh@gmail.com>
2019-02-03 14:26:18 +0530

    code optimizations

--- background.js
+++ background.js
@@ -1,29 +1,31 @@
-var notificationMessage = "Hey buddy, You should drink some water.";
-var notificationTitle = "Stay hidrated!";
-var timeInterval = 15; 
+const notificationMessage = 'Hey buddy, You should drink some water.';
+const notificationTitle = 'Stay hydrated!';
+var timeInterval = 15;
 // var timeInterval = 0.5;
 
 restartAlarms();
 browser.runtime.onMessage.addListener(handleMessage);
 
 function handleMessage(request, sender, sendResponse) {
-  // console.log(" Time: " + request.time);
-  timeInterval= request.time; 
-  // timeInterval= request.time/30;
-  sendResponse({response: "Time received successfully"});
+  timeInterval = request.time;
+  sendResponse({
+    response: 'Time received successfully'
+  });
   restartAlarms();
 }
 
-browser.alarms.onAlarm.addListener(function(alarm) {
-	browser.notifications.create("waterNotification",{
-	  "type": "basic",
-	  "iconUrl": "icons/bottle.png",
-	  "title": notificationTitle,
-	  "message": notificationMessage
-	});
+browser.alarms.onAlarm.addListener(function (alarm) {
+  browser.notifications.create('waterNotification', {
+    'type': 'basic',
+    'iconUrl': 'icons/bottle.png',
+    'title': notificationTitle,
+    'message': notificationMessage
+  });
 });
 
-function restartAlarms(){
-	browser.alarms.clearAll();
-	browser.alarms.create("waterReminder", {periodInMinutes: timeInterval});	
+function restartAlarms() {
+  browser.alarms.clearAll();
+  browser.alarms.create('waterReminder', {
+    periodInMinutes: timeInterval
+  });
 }
--- popup/popup.js
+++ popup/popup.js
@@ -1,34 +1,35 @@
-window.onload=()=>{
-	console.log("Popup loaded:" +localStorage.getItem('time'));
-	if(localStorage.getItem('time')==null){
-		localStorage.setItem('time',15);
-		document.getElementById("time-15").checked=true;
-	}else{
-		let checkedId="time-"+localStorage.getItem('time');
-		document.getElementById(checkedId).checked= true;
-	}
-}
+window.onload = () => {
+    if (localStorage.getItem('time') == null) {
+      let time = localStorage.getItem('time');
+      if (time == null) {
+        localStorage.setItem('time', 15);
+        document.getElementById('time-15').checked = true;
+      } else {
+        let checkedId = 'time-' + time;
+        document.getElementById(checkedId).checked = true;
+      }
+    }
 
-var radios = document.forms["time-form"].elements["time"];
-for(var i = 0, max = radios.length; i < max; i++) {
-  radios[i].onclick = function() {
-    console.log("Time:"+this.value);
-    localStorage.setItem('time',this.value);
-    setTime(parseInt(this.value));
-  }
-}
+    var radios = document.forms['time-form'].elements['time'];
+    for (var i = 0, max = radios.length; i < max; i++) {
+      radios[i].onclick = function () {
+        localStorage.setItem('time', this.value);
+        setTime(Number(this.value));
+      }
+    }
 
-function setTime(time){
-	console.log("Time:"+time);
-	var message=browser.runtime.sendMessage({"time": time});
-	message.then(handleSuccess, handleError);
-}
+    function setTime(time) {
+      var message = browser.runtime.sendMessage({
+        'time': time
+      });
+      // message.then(handleSuccess, handleError);
+    }
 
 
-function handleSuccess(message) {
-  console.log("Message from the background script:" + message.response);
-}
+    function handleSuccess(message) {
+      console.log('Message from the background script:' + message.response);
+    }
 
-function handleError(error) {
-  console.log("Error:"+error);
-}
+    function handleError(error) {
+      console.log('Error:' + error);
+    }
