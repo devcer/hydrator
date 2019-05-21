a6eb8b7efafa6e817d41316ca08110dcebf5e3a4
devcer <viswanathamsantosh@gmail.com>
2019-05-21 10:38:51 +0530

    few bug fixes

--- popup/popup.js
+++ popup/popup.js
@@ -1,35 +1,31 @@
 window.onload = () => {
-    if (localStorage.getItem('time') == null) {
-      let time = localStorage.getItem('time');
-      if (time == null) {
-        localStorage.setItem('time', 15);
-        document.getElementById('time-15').checked = true;
-      } else {
-        let checkedId = 'time-' + time;
-        document.getElementById(checkedId).checked = true;
-      }
+  if (localStorage.getItem('time') !== null) {
+    let time = localStorage.getItem('time');
+    if (time == null) {
+      localStorage.setItem('time', 15);
+      document.getElementById('time-15').checked = true;
+    } else {
+      let checkedId = 'time-' + time;
+      document.getElementById(checkedId).checked = true;
     }
+  }
 
-    var radios = document.forms['time-form'].elements['time'];
-    for (var i = 0, max = radios.length; i < max; i++) {
-      radios[i].onclick = function () {
-        localStorage.setItem('time', this.value);
-        setTime(Number(this.value));
-      }
+  var radios = document.forms['time-form'].elements['time'];
+  for (var i = 0, max = radios.length; i < max; i++) {
+    radios[i].onclick = function () {
+      let time = this.value;
+      localStorage.setItem('time', time);
+      document.getElementById('status').innerText = 'Reminder updated to ' + time + ' mins';
+      setTimeout(() => {
+        document.getElementById('status').innerText = '';
+      }, 5000);
+      setTime(Number(this.value));
     }
+  }
 
-    function setTime(time) {
-      var message = browser.runtime.sendMessage({
-        'time': time
-      });
-      // message.then(handleSuccess, handleError);
-    }
-
-
-    function handleSuccess(message) {
-      console.log('Message from the background script:' + message.response);
-    }
-
-    function handleError(error) {
-      console.log('Error:' + error);
-    }
+  function setTime(time) {
+    browser.runtime.sendMessage({
+      'time': time
+    });
+  }
+};
