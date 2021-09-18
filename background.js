042fa8e0958620b3019e88275c076130d4daf08a
devcer <viswanathamsantosh@gmail.com>
2021-09-18 16:59:18 +0530

    code restructuring, handling event at bubble level

--- background.js
+++ background.js
@@ -1,52 +1,52 @@
 const facts = [
-  'Do you know adult humans are 60 percent water, and our blood is 90 percent water? 🌊',
-  'Drinking water instead of soda can help with weight loss! 🥛',
-  'Do you know drinking water reduces the chance of a hangover? 🍺',
-  'Drinking water helps in enhancing physical performance! 🏋',
-  'Drinking water improves your skin health and beauty! 💃',
-  'Drinking water instead of other liquids will help you in weight loss! 🧘‍♀️',
-  'Drinking more water may help relieve Constipation! 🚽',
-  'Pace yourself to approach half of your recommended consumption by midday! 🌞',
-  'Drinking water when you first get up helps you to improve your immune system! 💪',
-  'Do you know drinking water before a workout will protect you from dehydration? 🏋',
-  'Drinking water helps you to regulate Body temperature! 🚶‍♂️',
-  'Did you know that drinking water lubricates the joints? It can even help with your back pain 🎒',
-  'Drinking water helps maintaining your blood pressure 🅰️🅱️🆎🅾️',
-  'Your brain is strongly influenced by your hydration status, make sure to drink water regularly 🧠',
-  'Dehydration can trigger headaches and migraine, drinking water is the cheapest treatment 🧠',
-  'Choose water when eating out. Generally, you will save money and reduce calories 😉',
-  'Carry a water bottle wherever you go, it\'s the mother nature\'s health potion ❤️',
-  'By the time you feel thirsty, your body has lost more than 1% of its total water, so let’s not feel thirst! ⚠️',
-  'Drinking enough water everyday can help reduce heart disease and cancer. Water helps flush toxins out of your body 👩🏻‍⚕️',
-  'There is the same amount of water on Earth as there was when the planet was formed. Believe it or not, the water you drink could share the same molecules that dinosaurs drank 🦖'
+  "Do you know adult humans are 60 percent water, and our blood is 90 percent water? 🌊",
+  "Drinking water instead of soda can help with weight loss! 🥛",
+  "Do you know drinking water reduces the chance of a hangover? 🍺",
+  "Drinking water helps in enhancing physical performance! 🏋",
+  "Drinking water improves your skin health and beauty! 💃",
+  "Drinking water instead of other liquids will help you in weight loss! 🧘‍♀️",
+  "Drinking more water may help relieve Constipation! 🚽",
+  "Pace yourself to approach half of your recommended consumption by midday! 🌞",
+  "Drinking water when you first get up helps you to improve your immune system! 💪",
+  "Do you know drinking water before a workout will protect you from dehydration? 🏋",
+  "Drinking water helps you to regulate Body temperature! 🚶‍♂️",
+  "Did you know that drinking water lubricates the joints? It can even help with your back pain 🎒",
+  "Drinking water helps maintaining your blood pressure 🅰️🅱️🆎🅾️",
+  "Your brain is strongly influenced by your hydration status, make sure to drink water regularly 🧠",
+  "Dehydration can trigger headaches and migraine, drinking water is the cheapest treatment 🧠",
+  "Choose water when eating out. Generally, you will save money and reduce calories 😉",
+  "Carry a water bottle wherever you go, it's the mother nature's health potion ❤️",
+  "By the time you feel thirsty, your body has lost more than 1% of its total water, so let’s not feel thirst! ⚠️",
+  "Drinking enough water everyday can help reduce heart disease and cancer. Water helps flush toxins out of your body 👩🏻‍⚕️",
+  "There is the same amount of water on Earth as there was when the planet was formed. Believe it or not, the water you drink could share the same molecules that dinosaurs drank 🦖",
 ];
 const factsSize = facts.length;
-const notificationMessage = 'Hey buddy, you should drink some water.';
-const notificationTitle = 'Stay hydrated!';
+const notificationMessage = "Hey there, It's time to drink some water.";
+const notificationTitle = "Stay hydrated!";
 let timeInterval = 15;
 
 function restartAlarms() {
   browser.alarms.clearAll();
-  browser.alarms.create('waterReminder', {
-    periodInMinutes: timeInterval
+  browser.alarms.create("waterReminder", {
+    periodInMinutes: timeInterval,
   });
 }
 
 function handleMessage(request, sender, sendResponse) {
   timeInterval = request.time;
   sendResponse({
-    response: 'Time received successfully'
+    response: "Time received successfully",
   });
   restartAlarms();
 }
 
 browser.alarms.onAlarm.addListener(() => {
   const fact = facts[Math.floor(Math.random() * factsSize)];
-  browser.notifications.create('waterNotification', {
-    type: 'basic',
-    iconUrl: 'icons/bottle.png',
+  browser.notifications.create("waterNotification", {
+    type: "basic",
+    iconUrl: "icons/bottle.png",
     title: notificationTitle,
-    message: `${notificationMessage} \n ${fact}`
+    message: `${notificationMessage} \n ${fact}`,
   });
 });
 
--- popup/popup.js
+++ popup/popup.js
@@ -1,41 +1,45 @@
 function setTime(time) {
   browser.runtime.sendMessage({
-    time
+    time,
   });
 }
 
 window.onload = () => {
-  if (localStorage.getItem('time') !== null) {
-    const time = localStorage.getItem('time');
+  if (localStorage.getItem("time") !== null) {
+    const time = localStorage.getItem("time");
     if (time == null) {
-      localStorage.setItem('time', 15);
-      document.getElementById('time-15').checked = true;
+      localStorage.setItem("time", 15);
+      document.getElementById("time-15").checked = true;
     } else {
       const checkedId = `time-${time}`;
       document.getElementById(checkedId).checked = true;
     }
   }
 
-  const radios = document.forms['time-form'].elements['time'];
-  const statusText = document.getElementById('status');
+  const timeForm = document.getElementById("time-form");
+  const statusText = document.getElementById("status");
   // Reference for the timeout so we can clear it midway through
   let timeoutHandle;
-  for (let i = 0, max = radios.length; i < max; i += 1) {
-    radios[i].onclick = (ev) => {
-      if (timeoutHandle) { // reset remaining time
+  timeForm.addEventListener("click", (event) => {
+    const radioButtonContainer = event.target.closest(".form-check");
+    if (typeof radioButtonContainer !== null) {
+      const radioButton =
+        radioButtonContainer.querySelector(".form-check-input");
+      if (timeoutHandle) {
+        // reset remaining time
         window.clearTimeout(timeoutHandle);
       }
-      const time = Number(ev.target.value);
-      localStorage.setItem('time', time);
+      const time = Number(radioButton.value);
+      localStorage.setItem("time", time);
 
-      statusText.classList.remove('hidden');
+      statusText.classList.remove("hidden");
       statusText.innerText = `Reminder updated to \n ${time} mins`;
 
       timeoutHandle = setTimeout(() => {
-        statusText.classList.add('hidden');
-        statusText.innerText = '';
+        statusText.classList.add("hidden");
+        statusText.innerText = "";
       }, 4000);
       setTime(time);
     }
-  }
+  });
 };
